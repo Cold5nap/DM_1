7c83b2320faaf2e825879b7978c6442b2545ceba
Cold5nap <and,andr2013@yandex.ru>
2020-11-18 08:57:56 +0300

    123

--- src/Sort.java
+++ src/Sort.java
@@ -13,7 +13,7 @@ public class Sort {
     public int[] insertionSortArray(int[] array) {
         counter = 0;
         int[] arr = new int[array.length];
-        for (int i=0;i< arr.length;i++) {
+        for (int i = 0; i < arr.length; i++) {
             arr[i] = array[i];
         }
         if (arr.length == 1) return arr;
@@ -21,10 +21,11 @@ public class Sort {
         for (int i = 1; i < arr.length; i++) {
             for (int j = i; j > 0 && arr[j - 1] > arr[j]; j--) {
                 int temp = arr[j];
-                arr[j] = arr[j-1];
-                arr[j-1] = temp;
+                arr[j] = arr[j - 1];
+                arr[j - 1] = temp;
                 counter++;
             }
+
         }
         return arr;
     }
@@ -34,7 +35,7 @@ public class Sort {
         if (array.length == 1) return array;
         counter = 0;
         int[] arr = new int[array.length];
-        for (int i=0;i< arr.length;i++) {
+        for (int i = 0; i < arr.length; i++) {
             arr[i] = array[i];
         }
 
@@ -52,12 +53,111 @@ public class Sort {
                 counter++;
             }
             //сдвигаем
-            for (int j = i; j > mid ; j--) {
+            for (int j = i; j > mid; j--) {
                 int temp = arr[j];
-                arr[j] = arr[j-1];
-                arr[j-1] = temp;
+                arr[j] = arr[j - 1];
+                arr[j - 1] = temp;
             }
 
+        }
+        return arr;
+    }
+
+    // меняет элементы из пары проходя по все за цикл
+//Этот способ упорядочивает весьма шустро,
+// O(n2) – его наихудшая сложность.
+// В среднем по времени имеем O(n log n),
+// а лучшая даже, не поверите, O(n).
+    public int[] bubbleSort(int[] arr) {
+        counter = 0;
+        int[] a = newArray(arr);
+        boolean isReplacement;
+
+        while (true) {
+            isReplacement = false;
+
+            for (int i = 1; i < a.length; i++) {
+                counter++;
+                if (a[i] < a[i - 1]) {
+                    isReplacement = true;
+                    int temp = a[i - 1];
+                    a[i - 1] = a[i];
+                    a[i] = temp;
+                }
+            }
+            if (!isReplacement) {
+                return a;
+            }
+        }
+    }
+
+    private int increment(int[] inc, int size) {
+        int p1, p2, p3, s;
+
+        p1 = p2 = p3 = 1;
+        s = -1;
+
+        do {
+            if (++s % 2 != 0) {
+                inc[s] = 8 * p1 - 6 * p2 + 1;
+            } else {
+                inc[s] = 9 * p1 - 9 * p3 + 1;
+                p2 *= 2;
+                p3 *= 2;
+            }
+            p1 *= 2;
+        } while (3 * inc[s] < size);
+
+        return s > 0 ? --s : 0;
+    }
+
+    /*
+    приращение - расстояние между сортируемыми элементами, в зависимости от прохода.
+    В конце приращение всегда равно единице - метод завершается обычной сортировкой вставками,
+    но именно последовательность приращений определяет рост эффективности.
+
+    При использовании таких приращений среднее количество операций: O(n^7/6),
+    в худшем случае - порядка O(n^4/3).
+    Автор приращения Р.Седжвик.
+     */
+    public int[] shellSort(int[] arr) {
+        counter = 0;
+        int[] a = newArray(arr);
+
+        int inc, i, j, s, seq[] = new int[40];
+        s = increment(seq, a.length);
+        while (s >= 0) {
+            inc = seq[s--];
+
+            for (i = inc; i < a.length; i++) {
+                int temp = a[i];
+                for (j = i - inc; (j >= 0) && (a[j] > temp); j -= inc) {
+                    a[j + inc] = a[j];
+                    counter++;
+                }
+                a[j + inc] = temp;
+            }
+        }
+        /*
+        for (int step = a.length / 2; step > 0; step /= 2) {
+            for (int i = step; i < a.length ; i++) {
+                for (int j = i - step; j >= 0 && a[j] > a[j + step] ; j -= step) {
+                    int x = a[j];
+                    a[j] = a[j + step];
+                    a[j + step] = x;
+                    counter++;
+                }
+            }
+        }
+
+         */
+        return a;
+    }
+
+    private int[] newArray(int[] array) {
+        int[] arr = new int[array.length];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = array[i];
         }
         return arr;
     }
--- src/Start.java
+++ src/Start.java
@@ -1,51 +1,114 @@
+import java.io.PrintStream;
+
 public class Start {
+    private static final PrintStream so = System.out;
+
     public static void main(String[] args) {
         int[] arr = {1, 23, 4, 56, 7, 8, 9, -1, -59, 0, 34, -1};
-        int[] badArr = badArrContain400Elem();
+        int[] badArr = badArrContain12Elem();
+        int[] goodArr = goodArrContain12Elem();
+
         Sort sort = new Sort();
 
+        int[] bubbleSort = sort.bubbleSort(arr);
+        System.out.print("Неотсортированный массив из 12 эл.: ");
+        printArr(arr);
+        System.out.print("Отсортированный массив из 12 эл.: ");
+        printArr(bubbleSort);
+        System.out.println("Кол-во операций сравнений: " + sort.getCounter()+"\n");
+
+        System.out.print("Неотсортированный наихудший вариант из 12 эл.: ");
+        printArr(badArr);
+        int[] badBubbleSort = sort.bubbleSort(badArr);
+        System.out.print("Отсортированный наихудший вариант из 12 эл.: ");
+        printArr(badBubbleSort);
+        System.out.println("Кол-во операций сравнений: " + sort.getCounter()+"\n");
+
+        System.out.print("Неотсортированный наилудший вариант из 12 эл.: ");
+        printArr(goodArr);
+        int[] goodBubbleSort = sort.bubbleSort(goodArr);
+        System.out.print("Отсортированный наилудший вариант из 12 эл.: ");
+        printArr(goodBubbleSort);
+        System.out.println("Кол-во операций сравнений: " + sort.getCounter()+"\n");
+
+        so.println("\n"+"---------------Сортировка Шелла---------------"+"\n");
+
+        int[] shellSort = sort.shellSort(arr);
+        System.out.print("Неотсортированный массив из 12 эл.: ");
+        printArr(arr);
+        System.out.print("Отсортированный массив из 12 эл.: ");
+        printArr(shellSort);
+        System.out.println("Кол-во операций сравнений: " + sort.getCounter()+"\n");
+
+        System.out.print("Неотсортированный наихудший вариант из 12 эл.: ");
+        printArr(badArr);
+        int[] badShellSort = sort.insertionSortArray(badArr);
+        System.out.print("Отсортированный наихудший вариант из 12 эл.: ");
+        printArr(badShellSort);
+        System.out.println("Кол-во операций сравнений: " + sort.getCounter()+"\n");
+
+        System.out.print("Неотсортированный наилудший вариант из 12 эл.: ");
+        printArr(goodArr);
+        int[] goodShellSort = sort.shellSort(goodArr);
+        System.out.print("Отсортированный наилудший вариант из 12 эл.: ");
+        printArr(goodShellSort);
+        System.out.println("Кол-во операций сравнений: " + sort.getCounter()+"\n");
+
+
+    }
+
+    private static void insertion(int[] arr, int[] badArr) {
+        Sort sort = new Sort();
         System.out.print("Неотсортированный массив из 12 эл.: ");
         printArr(arr);
         int[] insertionArr = sort.insertionSortArray(arr);
         System.out.print("Отсортированный массив из 12 эл.: ");
         printArr(insertionArr);
-        System.out.println("Кол-во операций сравнений: "+sort.getCounter());
+        System.out.println("Кол-во операций сравнений: " + sort.getCounter());
 
         System.out.print("Неотсортированный наихудший массив из 400 эл.: ");
         printArr(badArr);
         int[] badInsertionArr = sort.insertionSortArray(badArr);
         System.out.print("Отсортированный наихудший массив из 400 эл.: ");
         printArr(badInsertionArr);
-        System.out.println("Кол-во операций сравнений: "+sort.getCounter());
+        System.out.println("Кол-во операций сравнений: " + sort.getCounter());
 
         System.out.print("Неотсортированный массив из 12 эл.: ");
         printArr(arr);
         int[] binaryInsertionSortArray = sort.binaryInsertionSortArray(arr);
         System.out.print("Отсортированный массив из 12 эл.: ");
         printArr(binaryInsertionSortArray);
-        System.out.println("Кол-во операций сравнений: "+sort.getCounter());
+        System.out.println("Кол-во операций сравнений: " + sort.getCounter());
 
         System.out.print("Неотсортированный наихудший массив из 400 эл.: ");
         printArr(badArr);
         int[] badBinaryInsertionArr = sort.binaryInsertionSortArray(badArr);
         System.out.print("Отсортированный наихудший массив из 400 эл.: ");
         printArr(badBinaryInsertionArr);
-        System.out.println("Кол-во операций сравнений: "+sort.getCounter());
+        System.out.println("Кол-во операций сравнений: " + sort.getCounter());
     }
 
     private static void printArr(int[] arr) {
         System.out.println();
-        for (int i=0;i<arr.length;i++) {
-            System.out.print(arr[i]+ " ");
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
         }
         System.out.println();
     }
 
-    private static int[] badArrContain400Elem() {
-        int[] arrBad = new int [400];
-        int number =400;
-        for (int i=0;i<arrBad.length;i++) {
-            arrBad[i]=number;
+    private static int[] goodArrContain12Elem() {
+        int[] arrBad = new int[12];
+        for (int i = 1; i < arrBad.length; i++) {
+            arrBad[i] = i;
+        }
+        return arrBad;
+    }
+
+    private static int[] badArrContain12Elem() {
+        int[] arrBad = new int[12];
+        int number = 12;
+        for (int i = 0; i < arrBad.length; i++) {
+            arrBad[i] = number;
             number--;
         }
         return arrBad;
